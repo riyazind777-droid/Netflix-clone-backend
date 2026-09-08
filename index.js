17943d984ee0860f438a1ea4c535ad8b039fd4ff
riyazind777-droid <riyazind777@gmail.com>
2026-09-08 15:24:00 +0530

    updated error

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// WARNING: Serverless environments wipe global variables when they scale down.
-// Consider connecting a real database (like MongoDB Atlas) later.
+
 let users = [
     {
         username: "demo@gmail.com",
@@ -16,7 +15,7 @@ let users = [
     }
 ];
 
-// SIGNUP
+
 app.post("/signup", (req, res) => {
     console.log("Signup data received:", req.body);
 
@@ -30,24 +29,24 @@ app.post("/signup", (req, res) => {
     });
 });
 
-// LOGIN
+
 app.get("/login", (req, res) => {
     console.log("Login user lookup:", req.query.username);
 
-    let userFound = users.some(user => 
-        user.username === req.query.username && 
-        user.password === req.query.password
+    const userFound = users.some(
+        user =>
+            user.username === req.query.username &&
+            user.password === req.query.password
     );
 
     res.send(userFound);
 });
 
-// CRUCIAL: Only run app.listen locally. Do NOT block Vercel deployment.
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(5000, () => {
-        console.log("Server running locally on port 5000");
-    });
-}
 
-// CRUCIAL: Export the app module so Vercel can process it as a function!
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 module.exports = app;
